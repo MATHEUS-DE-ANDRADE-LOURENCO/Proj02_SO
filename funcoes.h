d2148e04440f3d3cada80a1bf0bf7b4dd257c43f
MATHEUS-DE-ANDRADE-LOURENCO <10419691@mackenzista.com.br>
2024-11-05 01:41:36 +0000

    recomecando

--- funcoes.h
+++ funcoes.h
@@ -1,39 +1,13 @@
-#ifndef FUNCOES_H
-#define FUNCOES_H
+# ifndef FUNCOES_H
 
-#include "Structs.h"
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <time.h>
+# define FUNCOES_H
 
-// Declaração da função para inicializar a memória física
-bool inicializarMemFisica(Frame memFisica[]);
+# include "Structs.h"
+# include <stdio.h>
+# include <stdlib.h>
 
-bool inicializarMemVirtual(Pagina memVirtual[]);
 
-bool inicializarProcesso(Processo *processo, int pid);
 
-bool alocarPaginasProc(int paginas_ids[], Processo *proc);
 
-bool inicializarPagina(Pagina *pagina, int pagina_id, int processo_id);
 
-void imprimirPagina(Pagina pag);
-
-void imprimirProcesso(Processo proc);
-
-void inicializarTabelaPaginas(Processo *proc);
-
-void colocarProcessoNoDisco(Processo discoRigido[TAM_VET], Processo *p);
-
-bool inicializarFrame(Frame *frame, Pagina *pag, int id);
-
-void alocarFrameNaMemoria(Frame *frame, Frame memoria[TAM_VET], int id);
-
-void colocarPaginaEmMemVirtual(Processo *proc, Pagina memVirtual[TAM_VET]);
-
-bool temPageFault(Pagina memVirtual[TAM_VET], Frame memFisica[TAM_VET]);
-
-void imprimirFrame(Frame frame);
-
-#endif
+# endif
--- main.c
+++ main.c
@@ -1,60 +1,21 @@
 # include "funcoes.h"
 
-int main() {
-    // Inicializa as memórias Principal e Virtual
-    Frame memPrincipal[TAM_VET];
-    Pagina memVirtual[TAM_VET];
-    Processo discoRigido[TAM_VET];
+int main(int argc, char *argv[]) {
 
-    if(inicializarMemFisica(memPrincipal)) {
-        printf("\nMemória física inicializada com sucesso!");
+    if(argc != 3) {
+        printf("Favor especificar arquivos da forma correta.");
+        return 1;
+    }
+
+    FILE *params = fopen(argv[1], "r");
+    FILE *exec = fopen(argv[2], "r");
+
+    if(params == NULL || exec == NULL) {
+        printf("\nHouve um erro na abertura de algum dos arquivos.");
+        return 1;
     } else {
-        printf("\nHouve um erro ao inicializar a memória física!");
+        printf("\nArquivos abertos com sucesso.");
     }
-
-    if(inicializarMemVirtual(memVirtual)) {
-        printf("\nMemória virtual inicializada com sucesso!");
-    } else {
-        printf("\nHouve um erro ao inicializar a memória virtual!");
-    }
-
-    // Cria um processo de três páginas, com o conteúdo ids presentes no vetor idPaginasP1.
-    
-    Processo p1;
-
-    int idPaginasP1[PAGS_PROC] = {2, 1, 4};
-
-    inicializarProcesso(&p1, 12);
-    
-    // Insere as páginas do vetor no processo
-    alocarPaginasProc(idPaginasP1, &p1);
-
-    // Inicializa a tabela de páginas do processo
-
-    inicializarTabelaPaginas(&p1);
-    
-    // Coloca as páginas do processo p1 na memória virtual.
-    colocarPaginaEmMemVirtual(&p1, memVirtual);
-    
-    
-    // Cria um frame para cada página da memória virtual.
-    for(int i = 0; i < TAM_VET; i++) {
-
-        Frame frame;
-        int idFisico = p1.tabelaPaginas[memVirtual[i].id];
-       
-        inicializarFrame(&frame, &memVirtual[i], idFisico);
-
-        alocarFrameNaMemoria(&frame, memPrincipal, idFisico);
-
-        imprimirFrame(memPrincipal[idFisico]);
-    }
-
-    
-
-    if(temPageFault(memVirtual, memPrincipal)) printf("\nPAGE FAULT: Sim");
-    else printf("\nPage Fault: Não");
-
 
     return 0;
 }
