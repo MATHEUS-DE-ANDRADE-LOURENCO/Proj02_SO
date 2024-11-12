c11891397e11ceaed523c4cc89611f003c13b45e
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2024-11-11 21:28:05 -0300

    Update main.c

--- main.c
+++ main.c
@@ -29,5 +29,19 @@ int main(int argc, char *argv[]) {
         printf("Processo %d, Página %d\n", execucao[i], execucao[i + 1]);
     }
 
+    for (int i = 0; execucao[i] != -1; i += 2) {
+        int processoId = execucao[i];
+        int paginaVirtual = execucao[i + 1];
+    
+        int enderecoFisico = gerenciarPagina(processoId, paginaVirtual);
+        if (enderecoFisico == -1) {
+            // Falta de página
+            lidarComFaltaDePaginaFIFO(processoId, paginaVirtual);
+        } else {
+            printf("Acesso à memória: Processo %d, Página Virtual %d, Frame %d\n", processoId, paginaVirtual, enderecoFisico);
+            registrarLog("Acesso à Memória", processoId, paginaVirtual, enderecoFisico);
+        }
+    }
+
     return 0;
 }
