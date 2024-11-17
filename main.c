e7270de15c70c4d3dc097372dff16414d7a4dbec
MATHEUS-DE-ANDRADE-LOURENCO <10419691@mackenzista.com.br>
2024-11-17 13:24:48 +0000

    ultimos ajustes

--- main.c
+++ main.c
@@ -88,79 +88,81 @@ int main(int argc, char *argv[]) {
 
     // Para cada elemento do vetor de execução
     int posMemReal = 0;
-for (int t = 0; execucao[t] != -1; t += 2) {
+    for (int t = 0; execucao[t] != -1; t += 2) {
 
-    printf("\nTempo t = %d", t/2);
+        printf("\nTempo t = %d", t/2);
 
-    int procAnalisado = execucao[t];
-    int pagAnalisada = execucao[t + 1];
-    int paginaEncontrada = 0;  // Flag para saber se a página foi encontrada
-    Pagina pagAtual;
+        int procAnalisado = execucao[t];
+        int pagAnalisada = execucao[t + 1];
+        int paginaEncontrada = 0;  // Flag para saber se a página foi encontrada
+        Pagina pagAtual;
 
-    // Procurar a página analisada na Memória Virtual
-    // Procurar o processo da página
-    for (int j = 0; j < TAM_VET && memVirtual[j].pid != -1; j++) {
-        // Se encontrar o processo analisado, procurar a página analisada.
-        if (memVirtual[j].pid == procAnalisado) {
-            for (int k = 0; k < tamanhoPaginasProcesso; k++) {
-                // Verifica se a página atual é a página analisada
-                if (memVirtual[j].paginas[k].id == pagAnalisada) {
-                    pagAtual = memVirtual[j].paginas[k];
-                    paginaEncontrada = 1;
-                    break;  // Saímos do loop de páginas
+        // Procurar a página analisada na Memória Virtual
+        // Procurar o processo da página
+        for (int j = 0; j < TAM_VET && memVirtual[j].pid != -1; j++) {
+            // Se encontrar o processo analisado, procurar a página analisada.
+            if (memVirtual[j].pid == procAnalisado) {
+                for (int k = 0; k < tamanhoPaginasProcesso; k++) {
+                    // Verifica se a página atual é a página analisada
+                    if (memVirtual[j].paginas[k].id == pagAnalisada) {
+                        pagAtual = memVirtual[j].paginas[k];
+                        paginaEncontrada = 1;
+                        break;  // Saímos do loop de páginas
+                    }
                 }
             }
-        }
 
-        // Se já encontramos a página, podemos sair do loop de processos
-        if (paginaEncontrada) {
-            break;
-        }
-    }
-
-    // Verificação se a página foi encontrada
-    if (!paginaEncontrada) {
-        printf("Página %d do Processo %d não encontrada na Memória Virtual\n", pagAnalisada, procAnalisado);
-    } else {
-        // Variável para preencher a memória real
-        Frame ocupaMemoria;
-        // Cria um frame que corresponde a uma página
-
-        ocupaMemoria.id = pagAtual.processoId;
-        ocupaMemoria.pagina_id = pagAtual.id;
-
-        //Como o frame ocupará a memória o atributo já será marcado como 1.
-        ocupaMemoria.ocupado = 1;
-
-        bool pgFault = pageFault(pagAtual, memReal, tamanhoMemoriaReal);
-        
-        if(pgFault) {
-            pagAtual.paginaReal = posMemReal % 4;
-            printf("\n[PAGE FAULT] Página %d do processo %d não está na memória", pagAtual.id, pagAtual.processoId);
-            if(memReal[posMemReal % 4].ocupado == 0) {
-                printf("\nCarregando página %d do processo %d (endereço lógico %d) no frame %d", pagAtual.id, pagAtual.processoId, pagAtual.paginaVirtual, pagAtual.paginaReal);
-            } else {
-                printf("\nSubstituindo página %d do processo %d (endereço lógico %d) no frame %d", pagAtual.id, pagAtual.processoId, pagAtual.paginaVirtual, pagAtual.paginaReal);
+            // Se já encontramos a página, podemos sair do loop de processos
+            if (paginaEncontrada) {
+                break;
             }
-
-            memReal[posMemReal%4] = ocupaMemoria;
-            posMemReal++;
-
-            printf("\nEstado da memória física: ");
-            imprimeMemoriaReal(memReal, tamanhoMemoriaReal);
-
-
-        } else {
-            printf("\nPágina %d do processo %d está na memória física.", pagAtual.id, pagAtual.processoId);
-            printf("\nPágina %d do processo %d (endereço lógico %d) no frame %d", pagAtual.id, pagAtual.processoId, pagAtual.paginaVirtual, buscaFrame(pagAtual.processoId, pagAtual.id, memReal, tamanhoMemoriaReal));
-
-            printf("\nEstado da memória física: ");
-            imprimeMemoriaReal(memReal, tamanhoMemoriaReal);            
         }
+
+        // Verificação se a página foi encontrada
+        if (!paginaEncontrada) {
+            printf("Página %d do Processo %d não encontrada na Memória Virtual\n", pagAnalisada, procAnalisado);
+        } else {
+            // Variável para preencher a memória real
+            Frame ocupaMemoria;
+            // Cria um frame que corresponde a uma página
+
+            ocupaMemoria.id = pagAtual.processoId;
+            ocupaMemoria.pagina_id = pagAtual.id;
+
+            //Como o frame ocupará a memória o atributo já será marcado como 1.
+            ocupaMemoria.ocupado = 1;
+
+            bool pgFault = pageFault(pagAtual, memReal, tamanhoMemoriaReal);
+            
+            if(pgFault) {
+                pagAtual.paginaReal = posMemReal % 4;
+                printf("\n[PAGE FAULT] Página %d do processo %d não está na memória", pagAtual.id, pagAtual.processoId);
+                if(memReal[posMemReal % 4].ocupado == 0) {
+                    printf("\nCarregando página %d do processo %d (endereço lógico %d) no frame %d", pagAtual.id, pagAtual.processoId, pagAtual.paginaVirtual, pagAtual.paginaReal);
+                } else {
+                    printf("\nSubstituindo página %d do processo %d (endereço lógico %d) no frame %d", pagAtual.id, pagAtual.processoId, pagAtual.paginaVirtual, pagAtual.paginaReal);
+                }
+
+                memReal[posMemReal%4] = ocupaMemoria;
+                posMemReal++;
+
+                printf("\nEstado da memória física: ");
+                imprimeMemoriaReal(memReal, tamanhoMemoriaReal);
+
+
+            } else {
+                printf("\nPágina %d do processo %d está na memória física.", pagAtual.id, pagAtual.processoId);
+                printf("\nPágina %d do processo %d (endereço lógico %d) no frame %d", pagAtual.id, pagAtual.processoId, pagAtual.paginaVirtual, buscaFrame(pagAtual.processoId, pagAtual.id, memReal, tamanhoMemoriaReal));
+
+                printf("\nEstado da memória física: ");
+                imprimeMemoriaReal(memReal, tamanhoMemoriaReal);            
+            }
+        }
+
+            
     }
 
-        
-}
+    printf("\nTodos os processos foram concluídos! Finalizando programa...");
 
 
     return 0;
